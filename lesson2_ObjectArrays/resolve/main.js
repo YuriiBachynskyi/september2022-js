9b31f9151f7786b130ec3ee93cc44fad61d79baa
YuriiBachynskyi <bachinskii911@gmail.com>
2022-11-14 21:35:15 +0200

    add lesson2 resolve

--- lesson2_ObjectArrays/resolve/main.js
+++ lesson2_ObjectArrays/resolve/main.js
@@ -91,12 +91,12 @@ let users = [
 ];
 
 console.log(users[0].password)
-console.log(users[1].password)
+console.log(users[1]['password'])
 console.log(users[2].password)
-console.log(users[3].password)
+console.log(users[3]['password'])
 console.log(users[4].password)
-console.log(users[5].password)
+console.log(users[5]['password'])
 console.log(users[6].password)
-console.log(users[7].password)
+console.log(users[7]['password'])
 console.log(users[8].password)
-console.log(users[9].password)
+console.log(users[9]['password'])
